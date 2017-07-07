3b0391148146e0a0d11682a0835ee6e586156597
harry <harry@madridui-iMac.local>
2017-07-07 14:55:21 +0900

    귀찮지만 커밋 미래의 나야 부탁한다

--- src/component/upload/upload.js
+++ src/component/upload/upload.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import UploadView from "./upload-view";
+import 'whatwg-fetch';
 
 export default class Upload extends Component {
     constructor(props) {
@@ -7,6 +8,7 @@ export default class Upload extends Component {
 
         this.onImageChange = this.onImageChange.bind(this);
         this.onUploadClick = this.onUploadClick.bind(this);
+        this.uploadImage = this.uploadImage.bind(this);
 
         this.state = {
             file: "",
@@ -15,7 +17,39 @@ export default class Upload extends Component {
     }
 
     onUploadClick() {
-        console.log(this.state.file, this.state.previewUrl);
+        let reader = new FileReader();
+
+        reader.onloadend = () => {
+            this.uploadImage(reader.result, this.state.file.type);
+        };
+
+        reader.readAsDataURL(this.state.file);
+    }
+
+    uploadImage(dataURL, fileType) {
+        let base64String = dataURL.split(",")[1];
+        let input = JSON.stringify({
+            base64Image: base64String,
+            fileType: fileType
+        });
+
+        let body = JSON.stringify({
+            input: input,
+            name: "firstExecution",
+            stateMachineArn: "arn:aws:states:us-east-1:516159972358:stateMachine:upload-face-image"
+        });
+
+        console.log(body);
+
+        fetch("https://bclbxxk175.execute-api.us-east-1.amazonaws.com/test/faceimage", {
+            mode: 'cors',
+            headers: {
+                "X-Api-Key": "o5F4feDYQHUzmkpQcOdH7cE7Gv3TpJ7606S8uLs1",
+                "Content-Type": "application/json"
+            },
+            method: 'POST',
+            body: body
+        });
     }
 
     onImageChange(imageFile) {
